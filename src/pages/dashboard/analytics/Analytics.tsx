eb59e99ce9e467f33792da32d9f0e351a8c10c87
FaribaZ <111224004+FaribaZ@users.noreply.github.com>
2023-08-25 16:31:29 +0330

    first commit

---
+++ src/pages/dashboard/analytics/Analytics.tsx
@@ -0,0 +1,5 @@
+const Data = () => {
+  return <div>Analytics</div>;
+};
+
+export default Data;
---
+++ src/pages/dashboard/data/Data.tsx
@@ -0,0 +1,5 @@
+const Analytics = () => {
+  return <div>Data</div>;
+};
+
+export default Analytics;
